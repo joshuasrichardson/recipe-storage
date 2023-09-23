9352ff39b46ba95b5ede52c41efe693fe0010efd
Joshua Richardson <joshuasrichardson1@gmail.com>
2023-09-22 23:53:24 -0600

    Remove anohter ?

--- back-end/src/helpers/string-helper.ts
+++ back-end/src/helpers/string-helper.ts
@@ -2,7 +2,7 @@ export const toListString = (
   list: Array<string>,
   conjunction: "and" | "or"
 ): string => {
-  if (!list?.length) return "";
+  if (!list.length) return "";
   if (list.length === 1) return list[0];
   if (list.length === 2) return `${list[0]} ${conjunction} ${list[1]}`;
   return `${list.slice(0, list.length - 1).join(", ")}, ${conjunction} ${
