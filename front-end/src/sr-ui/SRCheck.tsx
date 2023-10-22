e361e40e33a24768a51f8672c31567c7ecd4150d
Joshua Richardson <joshuasrichardson1@gmail.com>
2023-10-21 23:36:59 -0600

    Make check light color

--- front-end/src/sr-ui/SRCheck.tsx
+++ front-end/src/sr-ui/SRCheck.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { lightTextColor } from "./styles";
 
 const SRCheck = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
+    strokeWidth={2}
+    stroke={lightTextColor}
     className="w-6 h-6"
   >
     <path
